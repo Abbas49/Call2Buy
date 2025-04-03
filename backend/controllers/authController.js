e81d1caf61f8f59ef0c5b046f35450f3b6139876
Abbas49 <abbasabdo735@gmail.com>
2025-04-03 06:55:39 +0200

    Add user registration functionality with email validation and password hashing

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,33 @@
+import {supabase} from "../config/db.js"
+import bcrypt from "bcrypt"
+
 export const register = async (req, res) =>{
-    res.send("Regsiter")
+    try{
+        const {email, password, phone, user_address, full_name} = req.body;
+
+        // Check if email exists in the database
+        const user = await supabase.from("users").select().eq("email", email);
+        if(user.data && user.data.length != 0){
+            throw new Error("The provided email is already registered. Please use a different email or log in");
+        }
+
+        // Hash the password and insert user data into the database
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const result = await supabase.from("users").insert({
+            email: email,
+            password_hash: hashedPassword,
+            full_name: full_name,
+            phone: phone,
+            user_address: user_address
+        });
+        if(result.error){
+            throw new Error(result.error.message);
+        }
+
+        res.status(200).json({message: "Your account has been successfully created"});
+    }catch (err){
+        res.status(500).json({message: err.message});
+    }
 }
 
 export const login = async (req, res) =>{
