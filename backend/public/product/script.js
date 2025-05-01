b483cb47d1a57100f8a389570c5baebe9fe4002b
Abdallh Omran <171288488+AbdallhOmran@users.noreply.github.com>
2025-05-01 18:44:18 +0300

    fix: remove redundant initialization of hidden element display style

--- backend/public/product/script.js
+++ backend/public/product/script.js
@@ -1,7 +1,6 @@
 let submitBtn = document.querySelector(".submit-btn")
 let hid = document.querySelector(".num")
 
-// hid.style.display = "flex"
 submitBtn.addEventListener("click",()=>{
     if(hid.style.display === "flex"){
         hid.style.display = "none" 
