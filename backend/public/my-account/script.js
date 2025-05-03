7492eeda196c1a1e5ad6f8564c0dcad23e9b6ad7
Abdallh Omran <AbdallhOmran@users.noreply.github.com>
2025-05-04 02:52:41 +0300

    feat: add my account page with profile management and product listings

---
+++ backend/public/my-account/script.js
@@ -0,0 +1,7 @@
+let edit = document.querySelector(".edit");
+edit.addEventListener("click",()=> {
+    let main = document.querySelector(".main");
+    let accountInfo = document.querySelector(".cont-delete-info");
+    main.style.display = "block";
+    accountInfo.style.display = "none";
+})
