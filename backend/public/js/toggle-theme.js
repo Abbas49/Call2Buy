0616846cfcd1d876a63f454572b9ee7f0cbddeea
Abbas49 <abbasabdo735@gmail.com>
2025-05-11 23:19:09 +0300

    feat: enhance navigation and theme toggle functionality with improved transitions and theme persistence

--- backend/public/js/toggle-theme.js
+++ backend/public/js/toggle-theme.js
@@ -1,3 +1,6 @@
+const currentTheme = localStorage.getItem("theme") || "dark";
+document.documentElement.setAttribute("data-theme", currentTheme);
+
 let toggleButton = document.querySelector(".toggle-theme");
 
 toggleButton.addEventListener("click", (event)=>{
@@ -5,9 +8,11 @@ toggleButton.addEventListener("click", (event)=>{
     const currentTheme = html.getAttribute("data-theme");
     if(currentTheme==="dark"){
         html.setAttribute("data-theme", "light");
+        localStorage.setItem("theme", "light");
         toggleButton.innerText = "🌙"
     }else{
         html.setAttribute("data-theme", "dark");
+        localStorage.setItem("theme", "dark");
         toggleButton.innerText = "🔆"
     }
 })
